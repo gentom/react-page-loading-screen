97df0cade8fc587fe6b022ab87e199512cd6753e
gentom <gento.m.developer@gmail.com>
2018-04-01 22:28:55 +0900

    add page loading feature

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    const elem = document.getElementById('ipl-progress-indicator')
+    if(elem){
+      setTimeout(() => {
+        elem.classList.add('available')
+        setTimeout(() => {
+          elem.outerHTML = ''
+        }, 2000)
+      }, 2000)
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>React App</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
